1591e8c2e9521546020a2fb9886f29e1cf1887ee
mateosss <mateodemayo@gmail.com>
2015-11-21 15:11:03 -0300

    Pulido modulo de python, y agregado modulo xmpppy modificado

--- wololol/lol/chat/ejemplo.py
+++ wololol/lol/chat/ejemplo.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#Para simular ingreso de muchos usuarios al servidor descomente las lineas terminadas en "#"
 import Client
+#import time#
+#cont = 0#
 clientes = []
 nosalir = True
+cuentas = [
+("banersjk","zxmfkmk126"),
+("rayder38","caca123"),
+("Adriex","adriel1998"),
+("Bonzomontreux","bonzomontreux0"),
+("Bonzomontreuz","bonzomontreux0"),
+("mateosss","Cc:40987366"),
+]
 try:
     while nosalir:
+        #cont+=1#
+        #a = cuentas[cont%6][0]#
+        #b =cuentas[cont%6][1]#
+        #print("con "+str(cont))
         a = raw_input("usuario> ")
         b = raw_input("contraseña> ")
         if a == "send":
             to = raw_input("to> ")
             msg = raw_input("msg> ")
-            clientes[0].send(to, msg)
+            clientes[b].send(to, msg)
         elif a == "show":
             for i in clientes:
                 print(i.jid)
         else:
             cliente = Client.Cliente(a, b, "las")
-            clientes.append(cliente)
+            if cliente.connected:
+                for i in range(len(clientes)):
+                    if clientes[i].jid == cliente.jid:
+                        clientes.pop(i)
+                        break
+                clientes.append(cliente)
 except KeyboardInterrupt:
     for i in clientes:
-        i.close()
+        i.close("Cerrando "+str(i.name))
     print("Adios!")
 #cliente.send("421651", "Hola como andas?")#Envia Mensaje al summoner con esa id
 
