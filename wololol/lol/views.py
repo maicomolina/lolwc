7a59bf0a1e2f991b08f56c3fba9c3fb77b8332ec
mateosss <mateodemayo@gmail.com>
2015-12-06 22:21:32 -0300

    indicio de home con selector de imagen random funcionando, y busqueda funcional, ya es una pagina sin errores :3

--- wololol/lol/views.py
+++ wololol/lol/views.py
@@ -13,31 +13,12 @@ clientes = []
 
 def profile(request, summoner = None, idSum = None, region = None, info = None):
     context = RequestContext(request)
-    #si todo none, mostrar buscador
-    #si server e id/summoner, pedir get cache summoner
-    #si getCacheSummoner == None pedir api summoner
-    #getApiSummoner y reemplazar las variables en el html
-    # if request.method == "POST":
-    #     print(summoner, idSum, region, info)
-    #     info = getApiSummoner(summoner = summoner, idSum = idSum, region = region)
-    #     return HttpResponse(json.dumps(info), content_type="application/json")
     info = getSummoner(summoner = summoner, region = region)
     return render_to_response('profile.html', {"info":info}, context)
 
 def chat(request, user = None, password = None, region = None, friend = None):
     context = RequestContext(request)
     global clientes
-    #TODO iniciar chat cuando me logueo
-    #me logueo bien por html
-    #me logueo bien por url
-    #me logueo mal por html
-    #me logueo mal por url
-    #TODO darle al client.py capacidades de renderizar
-    #TODO que el client renderice los errores de auth y de conexion
-    #TODO enviar json con la informacion de getAll y printearla por python
-    #TODO printear la informacion json por javascript
-    #TODO loguearme con el chatoff
-    #TODO iniciar socket por javascript y el client, que el client le renderize al socket
     if request.method == "POST":
         if "message" in request.POST:
             if request.POST["message"] == "ready":
@@ -57,10 +38,7 @@ def chat(request, user = None, password = None, region = None, friend = None):
 
     if user != None and password != None and region != None:
         cliente = Cliente(user, password, region)
-        print(cliente.name)
-        print("POS CLIENTE")
         if cliente.connected:
-            print("CLIENTE CONNECTED")
             for i in range(len(clientes)):
                 if clientes[i].jid == cliente.jid:
                     clientes.pop(i)
@@ -81,19 +59,16 @@ def chat(request, user = None, password = None, region = None, friend = None):
                 }
 
             if request.method == "POST":
-                print("METODO POST RETURN")
                 return HttpResponse(json.dumps(info), content_type="application/json")
             return render_to_response('chat.html', {"info":info}, context)
 
         else:
-            print("ELSE CONNECTED")
             info={
                 #TODO returnear serverStatus
                 "message":"<h1 class='red-text'>No fue posible conectarse</h1><p class='center red-text'>Revisa tu usario y contraseña</p>",
                 "typ":"authError",
                 }
             if request.method == "POST":
-                print("ELSE CONNECTED POST")
                 return HttpResponse(json.dumps(info), content_type="application/json")
             return render_to_response('chat.html', {"info":info}, context)
     else:
@@ -101,10 +76,15 @@ def chat(request, user = None, password = None, region = None, friend = None):
 
 #TODO cambiar nombre static
 def data(request, section = None, specific = None):
-    print("#----data----#")
     context = RequestContext(request)
     return render_to_response('underConstruction.html', context)
 
 def home(request):
     context = RequestContext(request)
-    return render_to_response('home.html', context)
+    from random import randint
+    from django.contrib.staticfiles.templatetags.staticfiles import static
+    import os
+    url = static('img/home/')
+    amount = len(os.listdir("./lol/static/img/home"))
+    image = randint(1,amount)
+    return render_to_response('home.html',{"homeImage":str(image)}, context)
